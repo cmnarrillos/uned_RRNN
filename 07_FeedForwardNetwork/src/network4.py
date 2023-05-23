816c270f2d2cc4b5623458050a1e770019572de4
cmnarrillos <cmorenonarrillos@gmail.com>
2023-05-24 01:09:09 +0200

    Fix errors tf (now in different repository)

--- 07_FeedForwardNetwork/src/network4.py
+++ 07_FeedForwardNetwork/src/network4.py
@@ -74,9 +74,6 @@ def load_data_shared(filename="./data/mnist.pkl.gz"):
 #### Main class used to construct and train networks
 class Network(object):
     def __init__(self, layers, mini_batch_size):
-        """Takes a list of `layers`, describing the network architecture, and
-        a value for the `mini_batch_size` to be used during training
-        by stochastic gradient descent."""
         self.layers = layers
         self.mini_batch_size = mini_batch_size
         self.params = [param for layer in self.layers for param in layer.params]
@@ -85,9 +82,8 @@ class Network(object):
         init_layer = self.layers[0]
         init_layer.set_inpt(self.x, self.x, self.mini_batch_size)
         for j in range(1, len(self.layers)):
-            prev_layer, layer = self.layers[j - 1], self.layers[j]
-            layer.set_inpt(
-                prev_layer.output, prev_layer.output_dropout, self.mini_batch_size)
+            prev_layer, layer = self.layers[j-1], self.layers[j]
+            layer.set_inpt(prev_layer.output, prev_layer.output_dropout, self.mini_batch_size)
         self.output = self.layers[-1].output
         self.output_dropout = self.layers[-1].output_dropout
 
@@ -98,45 +94,59 @@ class Network(object):
         validation_x, validation_y = validation_data
         test_x, test_y = test_data
 
-        # compute number of minibatches for training, validation, and testing
-        num_training_batches = int(training_data[0].numpy().shape[0] / mini_batch_size)
-        num_validation_batches = int(validation_data[0].numpy().shape[0] / mini_batch_size)
-        num_test_batches = int(test_data[0].numpy().shape[0] / mini_batch_size)
+        num_training_batches = len(training_data) // mini_batch_size
+        num_validation_batches = len(validation_data) // mini_batch_size
+        num_test_batches = len(test_data) // mini_batch_size
 
         # define the (regularized) cost function, gradients, and updates
         l2_norm_squared = sum([tf.reduce_sum(tf.square(layer.w)) for layer in self.layers])
         cost = self.layers[-1].cost(self) + 0.5 * lmbda * l2_norm_squared / num_training_batches
-        grads = tf.gradients(cost, self.params)
-        updates = [(param, param - eta * grad)
-                   for param, grad in zip(self.params, grads)]
+        with tf.GradientTape() as tape:
+            grads = tape.gradient(cost, self.params)
+        updates = [(param, param - eta * grad) for param, grad in zip(self.params, grads)]
 
-        # define functions to train a mini-batch, and to compute the
-        # accuracy in validation and test mini-batches.
-        i = tf.placeholder(tf.int32)  # mini-batch index
+        i = tf.placeholder(tf.int32, shape=())
         train_mb = tf.function(
-            [i], cost, updates=updates,
-            givens={
-                self.x: training_x[i * self.mini_batch_size: (i + 1) * self.mini_batch_size],
-                self.y: training_y[i * self.mini_batch_size: (i + 1) * self.mini_batch_size]
-            })
+            lambda i: tf.keras.backend.function(
+                [self.x, self.y],
+                [cost],
+                updates=updates,
+                givens={
+                    self.x: training_x[i * self.mini_batch_size: (i + 1) * self.mini_batch_size],
+                    self.y: training_y[i * self.mini_batch_size: (i + 1) * self.mini_batch_size]
+                }
+            )
+        )
         validate_mb_accuracy = tf.function(
-            [i], self.layers[-1].accuracy(self.y),
-            givens={
-                self.x: validation_x[i * self.mini_batch_size: (i + 1) * self.mini_batch_size],
-                self.y: validation_y[i * self.mini_batch_size: (i + 1) * self.mini_batch_size]
-            })
+            lambda i: tf.keras.backend.function(
+                [self.x, self.y],
+                [self.layers[-1].accuracy(self.y)],
+                givens={
+                    self.x: validation_x[i * self.mini_batch_size: (i + 1) * self.mini_batch_size],
+                    self.y: validation_y[i * self.mini_batch_size: (i + 1) * self.mini_batch_size]
+                }
+            )
+        )
         test_mb_accuracy = tf.function(
-            [i], self.layers[-1].accuracy(self.y),
-            givens={
-                self.x: test_x[i * self.mini_batch_size: (i + 1) * self.mini_batch_size],
-                self.y: test_y[i * self.mini_batch_size: (i + 1) * self.mini_batch_size]
-            })
+            lambda i: tf.keras.backend.function(
+                [self.x, self.y],
+                [self.layers[-1].accuracy(self.y)],
+                givens={
+                    self.x: test_x[i * self.mini_batch_size: (i + 1) * self.mini_batch_size],
+                    self.y: test_y[i * self.mini_batch_size: (i + 1) * self.mini_batch_size]
+                }
+            )
+        )
         self.test_mb_predictions = tf.function(
-            [i], self.layers[-1].y_out,
-            givens={
-                self.x: test_x[i * self.mini_batch_size: (i + 1) * self.mini_batch_size]
-            })
-        # Do the actual training
+            lambda i: tf.keras.backend.function(
+                [self.x],
+                [self.layers[-1].y_out],
+                givens={
+                    self.x: test_x[i * self.mini_batch_size: (i + 1) * self.mini_batch_size]
+                }
+            )
+        )
+
         best_validation_accuracy = 0.0
         for epoch in range(epochs):
             for minibatch_index in range(num_training_batches):
@@ -145,19 +155,16 @@ class Network(object):
                     print("Training mini-batch number {0}".format(iteration))
                 cost_ij = train_mb(minibatch_index)
                 if (iteration + 1) % num_training_batches == 0:
-                    validation_accuracy = np.mean(
-                        [validate_mb_accuracy(j) for j in range(num_validation_batches)])
-                    print("Epoch {0}: validation accuracy {1:.2%}".format(
-                        epoch, validation_accuracy))
+                    validation_accuracy = np.mean([validate_mb_accuracy(j) for j in range(num_validation_batches)])
+                    print("Epoch {0}: validation accuracy {1:.2%}".format(epoch, validation_accuracy))
                     if validation_accuracy >= best_validation_accuracy:
                         print("This is the best validation accuracy to date.")
                         best_validation_accuracy = validation_accuracy
                         best_iteration = iteration
                         if test_data:
-                            test_accuracy = np.mean(
-                                [test_mb_accuracy(j) for j in range(num_test_batches)])
-                            print('The corresponding test accuracy is {0:.2%}'.format(
-                                test_accuracy))
+                            test_accuracy = np.mean([test_mb_accuracy(j) for j in range(num_test_batches)])
+                            print('The corresponding test accuracy is {0:.2%}'.format(test_accuracy))
+
         print("Finished training network.")
         print("Best validation accuracy of {0:.2%} obtained at iteration {1}".format(
             best_validation_accuracy, best_iteration))
